7564761bd3b2eb3b5eb47585d5a6d2771614e1cf
Cris-NTL <natolicristian92@gmail.com>
2023-03-29 12:54:50 +0200

    Code

--- js/script.js
+++ js/script.js
@@ -6,5 +6,10 @@ createApp({
             message: "Hello World!",
             img: "https://picsum.photos/300/300",
         }
+    },
+    methods: {
+        handleClick() {
+            alert(this.username)
+        }
     }
 }).mount("#app")
